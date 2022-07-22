98f1c04a82b17b562b9aa03896c4d7b198bac746
Sung-Hwan Lee <hwan.lee67@gmail.com>
2022-07-22 16:26:25 +0900

    Define well-formedness of time mapping

--- src/pf2ir/FutureCertify.v
+++ src/pf2ir/FutureCertify.v
@@ -26,6 +26,7 @@ Require Import Thread.
 Require Import PFCertify.
 
 Set Implicit Arguments.
+Set Nested Proofs Allowed.
 
 
 Module FutureCertify.
@@ -33,6 +34,53 @@ Module FutureCertify.
 
   Section FutureCertify.
     Variable f: map_t.
+
+    Definition map_eq: Prop :=
+      forall loc x y fx fy
+        (EQ: x = y)
+        (MAP1: f loc x fx)
+        (MAP2: f loc y fy),
+        fx = fy.
+
+    Definition map_eq_inv: Prop :=
+      forall loc x y fx fy
+        (EQ: fx = fy)
+        (MAP1: f loc x fx)
+        (MAP2: f loc y fy),
+        x = y.
+
+    Definition map_lt: Prop :=
+      forall loc x y fx fy
+        (LT: Time.lt x y)
+        (MAP1: f loc x fx)
+        (MAP2: f loc y fy),
+        Time.lt fx fy.
+
+    Definition map_lt_inv: Prop :=
+      forall loc x y fx fy
+        (LT: Time.lt fx fy)
+        (MAP1: f loc x fx)
+        (MAP2: f loc y fy),
+        Time.lt x y.
+
+    Definition map_complete (mem fmem: Memory.t): Prop :=
+      forall loc ts fts (MAP: f loc ts fts),
+        (<<COMPLETE: exists from to msg,
+            (<<GET: Memory.get loc to mem = Some (from, msg)>>) /\
+            (<<TS: ts = from \/ ts = to>>)>>) /\
+        (<<FCOMPLETE: exists ffrom fto fmsg,
+            (<<FGET: Memory.get loc fto fmem = Some (ffrom, fmsg)>>) /\
+            (<<FTS: fts = ffrom \/ fts = fto>>)>>)
+    .
+
+    Variant map_wf (mem fmem: Memory.t): Prop :=
+      | map_wf_intro
+          (MAP_EQ: map_eq)
+          (MAP_EQ_INV: map_eq_inv)
+          (MAP_LT: map_lt)
+          (MAP_LT_INV: map_lt_inv)
+          (MAP_COMPLETE: map_complete mem fmem)
+    .
 
     Definition timemap_map (tm ftm: TimeMap.t): Prop :=
       forall loc, f loc (tm loc) (ftm loc).
@@ -112,4 +160,23 @@ Module FutureCertify.
     Definition memory_map (max: TimeMap.t) (rsv: BoolMap.t) (mem fmem: Memory.t): Prop :=
       forall loc, memory_map_loc loc (max loc) (rsv loc) mem fmem.
   End FutureCertify.
+
+  Lemma memory_map_add
+        f1 max rsv mem1 fmem1
+        loc from to msg mem2
+        fmsg
+        (MAP1: memory_map f1 max rsv mem1 fmem1)
+        (WF1: map_wf f1 mem1 fmem1)
+        (ADD: Memory.add mem1 loc from to msg mem2)
+        (RESERVE: rsv loc = false -> Time.lt (max loc) from):
+    exists f2 ffrom fto fmem2,
+      (<<FADD: Memory.add fmem1 loc ffrom fto fmsg fmem2>>) /\
+      (<<F2: f2 = fun loc' ts fts =>
+                    (loc' = loc /\ ts = to /\ fts = fto) \/
+                    (loc' = loc /\ ts = from /\ fts = ffrom) \/
+                    f1 loc' ts fts>>) /\
+      (<<MAP2: message_map f2 msg fmsg -> memory_map f2 max rsv mem2 fmem2>>) /\
+      (<<WF2: map_wf f2 mem2 fmem2>>).
+  Proof.
+  Admitted.
 End FutureCertify.

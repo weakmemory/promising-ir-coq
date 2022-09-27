cb8e79c7dfe18d17510ba011f4bb76711ef9a55a
Sung-Hwan Lee <hwan.lee67@gmail.com>
2022-09-27 15:16:40 +0900

    WIP: PFConsistent

--- src/pf2ir/PFConsistent.v
+++ src/pf2ir/PFConsistent.v
@@ -30,7 +30,8 @@ Definition non_sc (e: ThreadEvent.t): Prop :=
   ~ ThreadEvent.is_sc e /\ ThreadEvent.get_machine_event e = MachineEvent.silent.
 #[export] Hint Unfold non_sc: core.
 
-Definition strict_pf (e: ThreadEvent.t): Prop := ~ ThreadEvent.is_racy_promise e.
+Definition strict_pf (e: ThreadEvent.t): Prop :=
+  ThreadEvent.is_pf e  /\ ~ ThreadEvent.is_racy_promise e.
 #[export] Hint Unfold strict_pf: core.
 
 
@@ -40,85 +41,65 @@ Module PFConsistent.
 
     Variant pf_consistent (th: Thread.t lang): Prop :=
       | pf_consistent_failure
-          pf e th1 th2
-          (STEPS: rtc (pstep (Thread.step (Memory.max_timemap (Global.memory (Thread.global th))) true)
-                             non_sc)
-                      (Thread.fully_reserved th) th1)
-          (STEP_FAILURE: Thread.step (Memory.max_timemap (Global.memory (Thread.global th))) pf e th1 th2)
+          e th1 th2
+          (STEPS: rtc (pstep (@Thread.step _) (ThreadEvent.is_pf /1\ non_sc)) (Thread.cap_of th) th1)
+          (STEP_FAILURE: Thread.step e th1 th2)
           (EVENT_FAILURE: ThreadEvent.get_machine_event e = MachineEvent.failure)
       | pf_consistent_fulfill
           th2
-          (STEPS: rtc (pstep (Thread.step (Memory.max_timemap (Global.memory (Thread.global th))) true)
-                             non_sc)
-                      (Thread.fully_reserved th) th2)
+          (STEPS: rtc (pstep (@Thread.step _) (ThreadEvent.is_pf /1\ non_sc)) (Thread.cap_of th) th2)
           (PROMISES: Local.promises (Thread.local th2) = BoolMap.bot)
     .
 
     Variant spf_consistent (th: Thread.t lang): Prop :=
       | spf_consistent_failure
-          pf e th1 th2
-          (STEPS: rtc (pstep (Thread.step (Memory.max_timemap (Global.memory (Thread.global th))) true)
-                             (strict_pf /1\ non_sc))
-                      (Thread.fully_reserved th) th1)
-          (STEP_FAILURE: Thread.step (Memory.max_timemap (Global.memory (Thread.global th))) pf e th1 th2)
+          e th1 th2
+          (STEPS: rtc (pstep (@Thread.step _) (strict_pf /1\ non_sc)) (Thread.cap_of th) th1)
+          (STEP_FAILURE: Thread.step e th1 th2)
           (EVENT_FAILURE: ThreadEvent.get_machine_event e = MachineEvent.failure)
           (EVENT_PF: strict_pf e)
       | spf_consistent_fulfill
           th2
-          (STEPS: rtc (pstep (Thread.step (Memory.max_timemap (Global.memory (Thread.global th))) true)
-                             (strict_pf /1\ non_sc))
-                      (Thread.fully_reserved th) th2)
+          (STEPS: rtc (pstep (@Thread.step _) (strict_pf /1\ non_sc)) (Thread.cap_of th) th2)
           (PROMISES: Local.promises (Thread.local th2) = BoolMap.bot)
     .
 
-    Variant spf_race (reserved: TimeMap.t) (th: Thread.t lang): Prop :=
+    Variant spf_race (th: Thread.t lang): Prop :=
       | spf_race_intro
-          pf e th1 th2
-          (STEPS: rtc (pstep (Thread.step reserved true) (strict_pf /1\ non_sc)) th th1)
-          (STEP_RACE: Thread.step reserved pf e th1 th2)
+          e th1 th2
+          (STEPS: rtc (pstep (@Thread.step _) (strict_pf /1\ non_sc)) th th1)
+          (STEP_RACE: Thread.step e th1 th2)
           (EVENT_RACE: ThreadEvent.is_racy_promise e)
     .
 
-    Variant pf_consistent_aux (reserved: TimeMap.t) (th: Thread.t lang): Prop :=
-      | pf_consistent_aux_failure
-          pf e th1 th2
-          (STEPS: rtc (pstep (Thread.step reserved true) non_sc) th th1)
-          (STEP_FAILURE: Thread.step reserved pf e th1 th2)
-          (EVENT_FAILURE: ThreadEvent.get_machine_event e = MachineEvent.failure)
-      | pf_consistent_aux_fulfill
-          th2
-          (STEPS: rtc (pstep (Thread.step reserved true) non_sc) th th2)
-          (PROMISES: Local.promises (Thread.local th2) = BoolMap.bot)
-    .
-
-    Variant non_sc_consistent (reserved: TimeMap.t) (th: Thread.t lang): Prop :=
+    Variant non_sc_consistent (th: Thread.t lang): Prop :=
       | non_sc_consistent_failure
-          pf e th1 th2
-          (STEPS: rtc (pstep (Thread.step_allpf reserved) non_sc) th th1)
-          (STEP_FAILURE: Thread.step reserved pf e th1 th2)
+          e th1 th2
+          (STEPS: rtc (pstep (@Thread.step _) non_sc) (Thread.cap_of th) th1)
+          (STEP_FAILURE: Thread.step e th1 th2)
           (EVENT_FAILURE: ThreadEvent.get_machine_event e = MachineEvent.failure)
       | non_sc_consistent_promises
           th2
-          (STEPS: rtc (pstep (Thread.step_allpf reserved) non_sc) th th2)
+          (STEPS: rtc (pstep (@Thread.step _) non_sc) (Thread.cap_of th) th2)
           (PROMISES: Local.promises (Thread.local th2) = BoolMap.bot)
     .
 
     Lemma rtc_tau_step_rtc_non_sc_step
-          reserved (th1 th2: Thread.t lang)
-          (STEPS: rtc (Thread.tau_step reserved) th1 th2):
+          (th1 th2: Thread.t lang)
+          (STEPS: rtc (@Thread.tau_step _) th1 th2):
       exists th2',
-        (<<STEPS1: rtc (pstep (Thread.step_allpf reserved) non_sc) th1 th2'>>) /\
+        (<<STEPS1: rtc (pstep (@Thread.step _) non_sc) th1 th2'>>) /\
         ((<<TH2: th2' = th2>>) \/
-         (<<STEPS2: rtc (Thread.tau_step reserved) th2' th2>>) /\
+         (<<STEPS2: rtc (@Thread.tau_step _) th2' th2>>) /\
          (<<PROMISES: Local.promises (Thread.local th2') = BoolMap.bot>>)).
     Proof.
       induction STEPS.
       { esplits; eauto. }
-      inv H. inv TSTEP.
+      inv H.
       destruct (classic (ThreadEvent.is_sc e)).
       - esplits; [refl|]. right. split.
         + econs 2; eauto. econs; eauto.
-        + inv STEP; inv LOCAL; ss. inv LOCAL0. auto.
+        + inv TSTEP; inv LOCAL; ss. inv LOCAL0. auto.
       - des.
         + esplits; [|eauto]. econs 2; eauto.
         + esplits; [|eauto]. econs 2; eauto.
@@ -127,9 +108,7 @@ Module PFConsistent.
     Lemma consistent_non_sc_consistent
           th
           (CONS: Thread.consistent th):
-      non_sc_consistent
-        (Memory.max_timemap (Global.memory (Thread.global th)))
-        (Thread.fully_reserved th).
+      non_sc_consistent th.
     Proof.
       inv CONS.
       - inv FAILURE.
@@ -141,42 +120,202 @@ Module PFConsistent.
         + econs 2; eauto.
     Qed.
 
+    Variant sim_memory (mem_src mem_tgt: Memory.t): Prop :=
+      | sim_memory_intro
+          (SOUND: Memory.le mem_src mem_tgt)
+          (COMPLETE: Memory.messages_le mem_tgt mem_src)
+    .
+
+    #[local] Program Instance sim_memory_PreOrder: PreOrder sim_memory.
+    Next Obligation.
+      ii. econs; refl.
+    Qed.
+    Next Obligation.
+      ii. inv H. inv H0. econs; etrans; eauto.
+    Qed.
+
+    Definition sim_greserves (rsv_src mem_src rsv_tgt mem_tgt: Memory.t): Prop :=
+      forall loc from to
+             (RSV_SRC: Memory.get loc to rsv_src = None)
+             (GRSV_SRC: Memory.get loc to mem_src = Some (from, Message.reserve)),
+        (<<RSV_TGT: Memory.get loc to rsv_tgt = None>>) /\
+        (<<GRSV_TGT: Memory.get loc to mem_tgt = Some (from, Message.reserve)>>).
+
     Variant sim_thread (th_src th_tgt: Thread.t lang): Prop :=
       | sim_thread_intro
           (STATE: Thread.state th_src = Thread.state th_tgt)
           (TVIEW: Local.tview (Thread.local th_src) = Local.tview (Thread.local th_tgt))
           (PROMISES: BoolMap.le (Local.promises (Thread.local th_src))
                                 (Local.promises (Thread.local th_tgt)))
-          (RESERVES: BoolMap.le (Local.reserves (Thread.local th_tgt))
-                                (Local.reserves (Thread.local th_src)))
+          (RESERVES: Memory.le (Local.reserves (Thread.local th_src))
+                               (Local.reserves (Thread.local th_tgt)))
           (SC: Global.sc (Thread.global th_src) = Global.sc (Thread.global th_tgt))
           (GPROMISES: BoolMap.minus (Global.promises (Thread.global th_src))
                                     (Local.promises (Thread.local th_src)) =
                       BoolMap.minus (Global.promises (Thread.global th_tgt))
                                     (Local.promises (Thread.local th_tgt)))
-          (GPROMISES_INV: BoolMap.minus (Local.promises (Thread.local th_src))
-                                        (Global.promises (Thread.global th_src)) =
-                          BoolMap.minus (Local.promises (Thread.local th_tgt))
-                                        (Global.promises (Thread.global th_tgt)))
-          (GRESERVES: forall loc (GET: Local.reserves (Thread.local th_src) loc = false),
-              Global.reserves (Thread.global th_tgt) loc = true)
-          (MEMORY: Global.memory (Thread.global th_src) = Global.memory (Thread.global th_tgt))
+          (GRESERVES: sim_greserves
+                        (Local.reserves (Thread.local th_src))
+                        (Global.memory (Thread.global th_src))
+                        (Local.reserves (Thread.local th_tgt))
+                        (Global.memory (Thread.global th_tgt)))
+          (MEMORY: sim_memory
+                     (Global.memory (Thread.global th_src))
+                     (Global.memory (Thread.global th_tgt)))
     .
 
-    Lemma fully_reserved_sim_thread th:
-      sim_thread (Thread.fully_reserved th) (Thread.fully_reserved th).
-    Proof.
-      econs; ss.
+    #[local] Program Instance sim_thread_PreOrder: PreOrder sim_thread.
+    Next Obligation.
+      ii. econs; ii; eauto; try refl.
+    Qed.
+    Next Obligation.
+      ii. inv H. inv H0. econs; try by (etrans; eauto).
+      ii. exploit GRESERVES; eauto. i. des. eauto.
     Qed.
 
-    Lemma sim_thread_internal_step
+    Lemma sim_memory_add
+      rsv_src rsv_tgt
+      mem1_src mem1_tgt
+      loc from to msg mem2_tgt
+      (MEM1: sim_memory mem1_src mem1_tgt)
+      (GRESERVES1: sim_greserves rsv_src mem1_src rsv_tgt mem1_tgt)
+      (ADD_TGT: Memory.add mem1_tgt loc from to msg mem2_tgt)
+      (MSG: msg <> Message.reserve):
+      exists mem2_src,
+        (<<ADD_SRC: Memory.add mem1_src loc from to msg mem2_src>>) /\
+        (<<MEM2: sim_memory mem2_src mem2_tgt>>) /\
+        (<<GRESERVES2: sim_greserves rsv_src mem2_src rsv_tgt mem2_tgt>>).
+    Proof.
+      inv MEM1.
+      exploit (@Memory.add_exists mem1_src loc from to msg); i.
+      { exploit SOUND; eauto. i.
+        exploit Memory.add_get1; eauto. i.
+        exploit Memory.add_get0; eauto. i. des.
+        exploit Memory.get_disjoint; [exact x1|exact GET0|].
+        i. des; subst; try congr.
+        symmetry. ss.
+      }
+      { inv ADD_TGT. inv ADD. ss. }
+      { inv ADD_TGT. inv ADD. ss. }
+      des. esplits; eauto.
+      - econs; ii.
+        + erewrite Memory.add_o; eauto.
+          revert LHS. erewrite Memory.add_o; try exact x0.
+          condtac; ss; eauto.
+        + erewrite Memory.add_o; eauto.
+          revert LHS. erewrite Memory.add_o; try exact ADD_TGT.
+          condtac; ss; eauto.
+      - ii. erewrite (@Memory.add_o mem2_tgt); eauto.
+        revert GRSV_SRC. erewrite (@Memory.add_o mem2); eauto.
+        condtac; ss; eauto. i. des. inv GRSV_SRC.
+        exploit Memory.add_get0; try exact x0. i. des. congr.
+    Qed.
+
+    Lemma sim_memory_reserve
+      rsv1_src mem1_src
+      rsv1_tgt mem1_tgt
+      loc from to rsv2_tgt mem2_tgt
+      (MEM1: sim_memory mem1_src mem1_tgt)
+      (RESERVES1: Memory.le rsv1_src rsv1_tgt)
+      (GRESERVES1: sim_greserves rsv1_src mem1_src rsv1_tgt mem1_tgt)
+      (RESERVE_TGT: Memory.reserve rsv1_tgt mem1_tgt loc from to rsv2_tgt mem2_tgt):
+      (<<MEM2: sim_memory mem1_src mem2_tgt>>) /\
+      (<<RESERVES2: Memory.le rsv1_src rsv2_tgt>>) /\
+      (<<GRESERVES2: sim_greserves rsv1_src mem1_src rsv1_tgt mem2_tgt>>).
+    Proof.
+      inv MEM1. inv RESERVE_TGT.
+      hexploit Memory.add_le; try exact MEM. i.
+      hexploit Memory.le_messages_le; eauto. i.
+      splits.
+      - econs; try by (etrans; eauto).
+        ii. revert LHS. erewrite Memory.add_o; eauto.
+        condtac; ss; eauto.
+      - etrans; eauto using Memory.add_le.
+      - ii. exploit GRESERVES1; eauto. i. des. splits; ss.
+        erewrite Memory.add_o; eauto.
+        condtac; ss; eauto. des. subst.
+        exploit Memory.add_get0; try exact MEM. i. des. congr.
+    Qed.
+
+    Lemma sim_memory_cancel
+      rsv1_src mem1_src
+      rsv1_tgt mem1_tgt
+      loc from to rsv2_tgt mem2_tgt
+      (MEM1: sim_memory mem1_src mem1_tgt)
+      (RESERVES1: Memory.le rsv1_src rsv1_tgt)
+      (GRESERVES1: sim_greserves rsv1_src mem1_src rsv1_tgt mem1_tgt)
+      (LE1_SRC: Memory.le rsv1_src mem1_src)
+      (CANCEL_TGT: Memory.cancel rsv1_tgt mem1_tgt loc from to rsv2_tgt mem2_tgt):
+      (<<MEM2: sim_memory mem1_src mem2_tgt>>) /\
+      (<<RESERVES2: Memory.le rsv1_src rsv2_tgt>>) /\
+      (<<GRESERVES2: sim_greserves rsv1_src mem1_src rsv1_tgt mem2_tgt>>) \/
+      exists rsv2_src mem2_src,
+        (<<CANCEL_SRC: Memory.cancel rsv1_src mem1_src loc from to rsv2_src mem2_src>>) /\
+        (<<MEM2: sim_memory mem2_src mem2_tgt>>) /\
+        (<<RESERVES2: Memory.le rsv2_src rsv2_tgt>>) /\
+        (<<GRESERVES2: sim_greserves rsv2_src mem2_src rsv2_tgt mem2_tgt>>).
+    Proof.
+      inv MEM1. inv CANCEL_TGT.
+      destruct (Memory.get loc to rsv1_src) as [[]|] eqn:RSV_SRC.
+      { right.
+        exploit Memory.remove_get0; try exact RSV. i. des.
+        exploit RESERVES1; eauto. i.
+        rewrite x0 in *. inv GET.
+        exploit LE1_SRC; eauto. i.
+        exploit Memory.remove_exists; try exact RSV_SRC. i. des.
+        exploit Memory.remove_exists; try exact x1. i. des.
+        esplits; eauto; ii.
+        - econs; ii.
+          + erewrite Memory.remove_o; eauto.
+            revert LHS. erewrite Memory.remove_o; try exact x3.
+            condtac; ss; eauto.
+          + erewrite Memory.remove_o; eauto.
+            revert LHS. erewrite Memory.remove_o; try exact MEM.
+            condtac; ss; eauto.
+        - erewrite Memory.remove_o; eauto.
+          revert LHS. erewrite Memory.remove_o; try exact x2.
+          condtac; ss; eauto.
+        - erewrite Memory.remove_o in RSV_SRC0; eauto.
+          erewrite Memory.remove_o in GRSV_SRC; eauto.
+          des_ifs. ss. guardH o.
+          exploit GRESERVES1; eauto. i. des.
+          exploit Memory.remove_get1; try exact GRSV_TGT; eauto. i. des; try congr.
+          splits; ss.
+          erewrite Memory.remove_o; eauto. condtac; ss.
+      }
+      { left.
+        destruct (Memory.get loc to mem1_src) as [[]|] eqn:GRSV_SRC.
+        { exploit Memory.remove_get0; try exact RSV. i. des.
+          exploit Memory.remove_get0; try exact MEM. i. des.
+          exploit SOUND; eauto. i.
+          rewrite x0 in *. inv GET1.
+          exploit GRESERVES1; eauto. i. des. congr.
+        }
+        splits; ii.
+        - hexploit Memory.remove_le; try exact MEM. i.
+          hexploit Memory.le_messages_le; try exact H. i.
+          econs; try by (etrans; eauto). ii.
+          erewrite Memory.remove_o; eauto. condtac; ss; eauto.
+          des. subst. congr.
+        - erewrite Memory.remove_o; eauto. condtac; ss; eauto.
+          des. subst. congr.
+        - exploit GRESERVES1; eauto. i. des.
+          erewrite (@Memory.remove_o mem2_tgt); eauto. condtac; ss.
+          des. subst. congr.
+      }
+    Qed.
+
+    (* TODO *)
+
+    Lemma sim_thread_non_pf_step
           th1_src
-          reserved e th1_tgt th2_tgt
+          e th1_tgt th2_tgt
           (SIM1: sim_thread th1_src th1_tgt)
-          (STEP_TGT: Thread.step reserved false e th1_tgt th2_tgt):
+          (STEP_TGT: Thread.step e th1_tgt th2_tgt)
+          (EVENT: ~ ThreadEvent.is_pf e):
       sim_thread th1_src th2_tgt.
     Proof.
-      inv SIM1. inv STEP_TGT. ss. inv LOCAL.
+      inv SIM1. inv STEP_TGT; inv LOCAL; ss.
       - inv LOCAL0. econs; ss.
         + etrans; eauto.
           inv PROMISE. eauto using BoolMap.add_le.
@@ -184,21 +323,14 @@ Module PFConsistent.
           eauto using Promises.promise_minus.
         + rewrite GPROMISES_INV.
           eauto using Promises.promise_minus_inv.
-      - inv LOCAL0. econs; ss.
-        + inv RESERVE. ii. revert LHS.
-          erewrite BoolMap.add_o; eauto. condtac; auto. i. subst.
-          destruct (Local.reserves (Thread.local th1_src) loc) eqn:GET; ss.
-          exploit GRESERVES; try exact GET. i.
-          exploit BoolMap.add_get0; try exact GADD. i. des. congr.
-        + i. inv RESERVE.
-          erewrite BoolMap.add_o; eauto. condtac; auto.
-      - inv LOCAL0. econs; ss.
-        + inv CANCEL. ii. revert LHS.
-          erewrite BoolMap.remove_o; eauto. condtac; ss. auto.
-        + i. inv CANCEL.
-          erewrite BoolMap.remove_o; eauto. condtac; ss; auto. subst.
-          exploit BoolMap.remove_get0; try exact REMOVE. i. des.
-          exploit RESERVES; try exact GET1. i. congr.
+      - inv LOCAL0. inv RESERVE. econs; ss.
+        + hexploit Memory.add_le; try exact RSV. i. etrans; eauto.
+        + inv MEMORY.
+          hexploit Memory.add_le; try exact MEM. i.
+          hexploit Memory.le_messages_le; try exact H. i.
+          econs; try by (etrans; eauto).
+          ii. revert LHS. erewrite Memory.add_o; eauto.
+          condtac; ss; eauto.
     Qed.
 
     Lemma sim_is_racy
@@ -269,19 +401,18 @@ Module PFConsistent.
           * eapply equal_f in GPROMISES_INV. rewrite GPROMISES_INV. ss.
     Qed.
 
-    Lemma sim_thread_program_step
-          reserved
+    Lemma sim_thread_pf_step
           th1_src
           e th1_tgt th2_tgt
           (SIM1: sim_thread th1_src th1_tgt)
-          (STEP_TGT: Thread.step reserved true e th1_tgt th2_tgt)
+          (STEP_TGT: Thread.step e th1_tgt th2_tgt)
           (NONSC: ~ ThreadEvent.is_sc e):
       exists th2_src,
-        (<<STEP_SRC: Thread.step reserved true e th1_src th2_src>>) /\
+        (<<STEP_SRC: Thread.step e th1_src th2_src>>) /\
         (<<SIM2: sim_thread th2_src th2_tgt>>).
     Proof.
-      destruct th1_src as [st1_src [tview1_src prm1_src rsv1_src] [sc1_src gprm1_src grsv1_src mem1_src]],
-          th1_tgt as [st1_tgt [tview1_tgt prm1_tgt rsv1_tgt] [sc1_tgt gprm1_tgt grsv1_tgt mem1_tgt]].
+      destruct th1_src as [st1_src [tview1_src prm1_src rsv1_src] [sc1_src gprm1_src mem1_src]],
+          th1_tgt as [st1_tgt [tview1_tgt prm1_tgt rsv1_tgt] [sc1_tgt gprm1_tgt mem1_tgt]].
       inv SIM1. ss. subst.
       inv STEP_TGT. inv LOCAL; ss.
       { (* silent *)
@@ -403,7 +534,7 @@ Module PFConsistent.
           th
           (CONS: pf_consistent_aux
                    (Memory.max_timemap (Global.memory (Thread.global th)))
-                   (Thread.fully_reserved th)):
+                   (Thread.cap_of th)):
       pf_consistent th.
     Proof.
       inv CONS.
@@ -443,7 +574,7 @@ Module PFConsistent.
       (<<SPF_CONS: spf_consistent th>>) \/
       (<<SPF_RACE: spf_race
                      (Memory.max_timemap (Global.memory (Thread.global th)))
-                     (Thread.fully_reserved th)>>).
+                     (Thread.cap_of th)>>).
     Proof.
       inv CONS.
       - exploit rtc_pf_steps_rtc_spf_steps; eauto. i. des; eauto.

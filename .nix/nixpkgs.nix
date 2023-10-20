414a8c09d52f966739234799c9c654c6ca4f4f91
InnocentusLime <InnocentusLime@github.com>
2023-10-20 07:20:03 +0000

    Added nix configs

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+         url = https://github.com/weakmemory/nixpkgs/archive/74f9c700bb92c7f7ba5052a2db9502b21b88f71a.tar.gz;
+         sha256 = "0d9jjn5mdky7np1mqbkis0dn4vj9vdxzdzrlfyqcm0znp1mjqd8l";
+       }

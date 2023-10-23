b441d94f9b1b96f2203904bec4a6c116dbf84b3a
Anton Podkopaev <podkoav239@gmail.com>
2023-10-23 18:32:25 +0200

    Removed all version of Coq from CI except for 8.18

--- .nix/config.nix
+++ .nix/config.nix
@@ -5,33 +5,6 @@
 
   default-bundle = "8.18";
 
-  bundles."8.15" = {
-    push-branches = [ "**" ];
-    coqPackages.coq.override.version = "8.15";
-    coqPackages.hahn.override.version = "master";
-    coqPackages.hahnExt.override.version = "main";
-    coqPackages.sflib.override.version = "master";
-    coqPackages.promising-lib.override.version = "master";
-    coqPackages.ITree.override.version = "master";
-  };
-  bundles."8.16" = {
-    push-branches = [ "**" ];
-    coqPackages.coq.override.version = "8.16";
-    coqPackages.hahn.override.version = "master";
-    coqPackages.hahnExt.override.version = "main";
-    coqPackages.sflib.override.version = "master";
-    coqPackages.promising-lib.override.version = "master";
-    coqPackages.ITree.override.version = "master";
-  };
-  bundles."8.17" = {
-    push-branches = [ "**" ];
-    coqPackages.coq.override.version = "8.17";
-    coqPackages.hahn.override.version = "master";
-    coqPackages.hahnExt.override.version = "main";
-    coqPackages.sflib.override.version = "master";
-    coqPackages.promising-lib.override.version = "master";
-    coqPackages.ITree.override.version = "master";
-  };
   bundles."8.18" = {
     push-branches = [ "**" ];
     coqPackages.coq.override.version = "8.18";
